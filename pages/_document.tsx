ecdf05a5f14ae4a4793c6db1aa1a5c1392acf20b
nikolai_job <ursegovnikolaj@gmail.com>
2023-03-19 15:51:27 +0300

    init project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
         <Head>
             <title>ТТК - промышленные запчасти и оборудование</title>
+            <link rel="icon" href="/favicon.svg" />
             <meta name="description" content="ООО TTK поставляет промышленные запасные части и оборудование для всех промышленных предприятий в РФ."/>
             <meta name="keywords" content="Промышленные продукты и индустриальный сервис, Запасные части, Электротехника,
 Гидравлика: OLAER, ORSTA, PARKER, FAWCET, VICKERS, CHRISTIE, HAWE, DENISON, BOSCH, REXROTH, HYDAC, BRINKMANN, EPE, INTEGRAL, EATON, KMK, AEROEL, SCHIEDRUM HYDRAULIK, CASAPPA, SAWA, SPECKEN, BOHLER, MARZOCCHI, MOTOVARIO, FRANK Электрика: SIEMENS, ALLEN-BRADLEY, BINDER, ZUMBACH, PILZ, SIKORA, PHOENIX CONTACT, MOELLER, EUROTHERM, CROUZET, RITAALL, KEB, LEXEL, IFM, WEM, LOHER, HELMKE, BONFIGLIOU, SEW, SITI, WAMEL, FRABA, BIELLONI, COEL-MOTORI, IPF, PIGNAT Автоматика: OMRON, SICK, BALLUFF, LEUZE, DATASENSOR, AECO, ELOBAU, END, CERV Пневматика: FESTO, SMC, HOERBIGER, MECMAN, NOVASERTIC, ALFA LAVAL, ORTUNGHAUS ЧПУ: HEIDENHEIN, SIEMENS, FANUC Фильтры: MANN, CUNO, PARKER, MAHLE, INTERNORMEN, HYDAC, FINFILTER, DONALDSON, US FILTER, STERIS, MPFILTRI Привода: MITSUBISHI, TOSHIBA, INDRAMAT, CONTROL TECHNIQUES, СANTRAVES DRIVES, HEIDENHAIN, ABB, SIEMENS, BAUMUELLER, BBS, GETTYS, BALDOR, SEV, ALLEN-BRADLEY, PETER KIEBACK Уплотнения: GARLOCK, BUSAK, SHAMBAN и под заказ Подшипники: INA, FAG, SKF, TIMKEN, ТТК, TTK"/>
